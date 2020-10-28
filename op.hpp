a079961ba46af51a7f7c5d6ab0d7a329311d4121
roycemoses <royce.kmoses@gmail.com>
2020-10-28 16:42:37 -0700

    added mock classes

--- op.hpp
+++ op.hpp
@@ -10,4 +10,44 @@ class Op : public Base {
         virtual std::string stringify() { return ""; }
 };
 
+class SevenPointFiveOpMock: public Base {
+    public:
+        SevenPointFiveOpMock() { };
+
+        virtual double evaluate() { return 7.5; }
+        virtual std::string stringify() { return "7.5"; }
+};
+
+class ZeroOpMock: public Base {
+    public:
+        ZeroOpMock() { };
+
+        virtual double evaluate() { return 0; }
+        virtual std::string stringify() { return "0"; }
+};
+
+class NegativeSevenPointFiveOpMock: public Base {
+    public:
+        NegativeSevenPointFiveOpMock() { };
+
+        virtual double evaluate() { return -7.5; }
+        virtual std::string stringify() { return "-7.5"; }
+};
+
+class OneThousandOpMock: public Base {
+    public:
+        OneThousandOpMock() { };
+
+        virtual double evaluate() { return 1000; }
+        virtual std::string stringify() { return "1000"; }
+};
+
+class NegativeOneThousandOpMock: public Base {
+    public:
+        NegativeOneThousandOpMock() { };
+
+        virtual double evaluate() { return -1000; }
+        virtual std::string stringify() { return "-1000"; }
+};
+
 #endif //__OP_HPP__
