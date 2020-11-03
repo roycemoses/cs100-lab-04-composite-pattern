3736b0ba521ca51fca91cfd6fd746f59f9dc9d9d
Danz Moses <danzmoses@gmail.com>
2020-11-02 16:56:52 -0800

    Fixed Op class to work with Base class

--- op.hpp
+++ op.hpp
@@ -4,52 +4,10 @@
 #include "base.hpp"
 
 class Op : public Base {
-    private:
-        double value;
     public:
-        Op(double value) : value(value), Base() { }
-        virtual double evaluate() { return value; }
+        Op(double value) : Base() { this->setValue(value); }
+        virtual double evaluate() { return this->getValue(); }
         virtual std::string stringify() { return std::to_string(evaluate()); }
-};
-
-class SevenPointFiveOpMock: public Base {
-    public:
-        SevenPointFiveOpMock() { };
-
-        virtual double evaluate() { return 7.5; }
-        virtual std::string stringify() { return "7.5"; }
-};
-
-class ZeroOpMock: public Base {
-    public:
-        ZeroOpMock() { };
-
-        virtual double evaluate() { return 0; }
-        virtual std::string stringify() { return "0"; }
-};
-
-class NegativeSevenPointFiveOpMock: public Base {
-    public:
-        NegativeSevenPointFiveOpMock() { };
-
-        virtual double evaluate() { return -7.5; }
-        virtual std::string stringify() { return "-7.5"; }
-};
-
-class OneThousandOpMock: public Base {
-    public:
-        OneThousandOpMock() { };
-
-        virtual double evaluate() { return 1000; }
-        virtual std::string stringify() { return "1000"; }
-};
-
-class NegativeOneThousandOpMock: public Base {
-    public:
-        NegativeOneThousandOpMock() { };
-
-        virtual double evaluate() { return -1000; }
-        virtual std::string stringify() { return "-1000"; }
 };
 
 #endif //__OP_HPP__
