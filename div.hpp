008069eb57d91ffa3cefbe9acf22b4e4ba5db25a
Danz Moses <danzmoses@gmail.com>
2020-11-02 17:39:19 -0800

    Implemented constructor for Div class

--- div.hpp
+++ div.hpp
@@ -8,7 +8,7 @@ class Div : public Base {
         Base* value1;
         Base* value2;
     public:
-        Div(Base* value1, Base* value2) : value1(value1), value2(value2), Base() { }
+        Div(Base* value1, Base* value2) : value1(value1), value2(value2), Base() { this->setValue(evaluate()); }
         virtual double evaluate() { return value1->getValue() / value2->getValue(); }
         virtual std::string stringify() { }
 };
