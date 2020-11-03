9f58b32c7e26f7719f3deed003b0fd9460cd426e
Danz Moses <danzmoses@gmail.com>
2020-11-02 17:15:13 -0800

    Fixed test cases for Add class

--- test.hpp
+++ test.hpp
@@ -8,62 +8,57 @@
 #include "sub.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
-    Op* test = new Op(8);
+    Base* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
 }
 
 TEST(AddTest, AddEvaluatePositive) {
-    SevenPointFiveOpMock* value1 = new SevenPointFiveOpMock();
-    SevenPointFiveOpMock* value2 = new SevenPointFiveOpMock();
+    Base* SevenPointFiveOp = new Op(7.5);
 
-    Add* add = new Add(value1->evaluate(), value2->evaluate());
+    Base* add = new Add(SevenPointFiveOp, SevenPointFiveOp);
     EXPECT_EQ(add->evaluate(), 15.0);
-    EXPECT_EQ(add->stringify(), "15.000000");
+    EXPECT_EQ(add->stringify(), "7.500000 + 7.500000");
 }
 
 TEST(AddTest, AddEvaluateNegativePlusZero) {
-    NegativeSevenPointFiveOpMock* value1 = new NegativeSevenPointFiveOpMock();
-    ZeroOpMock* value2 = new ZeroOpMock();
+    Base* NegativeSevenPointFiveOp = new Op(-7.5);
+    Base* ZeroOp = new Op(0);
 
-    Add* add = new Add(value1->evaluate(), value2->evaluate());
+    Base* add = new Add(NegativeSevenPointFiveOp, ZeroOp);
     EXPECT_EQ(add->evaluate(), -7.5);
-    EXPECT_EQ(add->stringify(), "-7.500000");
+    EXPECT_EQ(add->stringify(), "-7.500000 + 0.000000");
 }
 
 TEST(AddTest, AddEvaluateNegativePlusNegative) {
-    NegativeSevenPointFiveOpMock* value1 = new NegativeSevenPointFiveOpMock();
-    NegativeSevenPointFiveOpMock* value2 = new NegativeSevenPointFiveOpMock();
+    Base* NegativeSevenPointFiveOp = new Op(-7.5);
 
-    Add* add = new Add(value1->evaluate(), value2->evaluate());
+    Base* add = new Add(NegativeSevenPointFiveOp, NegativeSevenPointFiveOp);
     EXPECT_EQ(add->evaluate(), -15.0);
-    EXPECT_EQ(add->stringify(), "-15.000000");
+    EXPECT_EQ(add->stringify(), "-7.500000 + -7.500000");
 }
 
 TEST(AddTest, AddEvaluateZeroPlusZero) {
-    ZeroOpMock* value1 = new ZeroOpMock();
-    ZeroOpMock* value2 = new ZeroOpMock();
+    Base* ZeroOp = new Op(0);
 
-    Add* add = new Add(value1->evaluate(), value2->evaluate());
+    Base* add = new Add(ZeroOp, ZeroOp);
     EXPECT_EQ(add->evaluate(), 0);
-    EXPECT_EQ(add->stringify(), "0.000000");
+    EXPECT_EQ(add->stringify(), "0.000000 + 0.000000");
 }
 
 TEST(AddTest, AddEvaluateLargePositiveNumbers) {
-    OneThousandOpMock* value1 = new OneThousandOpMock();
-    OneThousandOpMock* value2 = new OneThousandOpMock();
+    Base* OneThousandOp = new Op(1000);
 
-    Add* add = new Add(value1->evaluate(), value2->evaluate());
+    Base* add = new Add(OneThousandOp, OneThousandOp);
     EXPECT_EQ(add->evaluate(), 2000.0);
-    EXPECT_EQ(add->stringify(), "2000.000000");
+    EXPECT_EQ(add->stringify(), "1000.000000 + 1000.000000");
 }
 
 TEST(AddTest, AddEvaluateLargeNegativeNumbers) {
-    NegativeOneThousandOpMock* value1 = new NegativeOneThousandOpMock();
-    NegativeOneThousandOpMock* value2 = new NegativeOneThousandOpMock();
+    Base* NegativeOneThousandOp = new Op(-1000);
 
-    Add* add = new Add(value1->evaluate(), value2->evaluate());
+    Base* add = new Add(NegativeOneThousandOp, NegativeOneThousandOp);
     EXPECT_EQ(add->evaluate(), -2000.0);
-    EXPECT_EQ(add->stringify(), "-2000.000000");
+    EXPECT_EQ(add->stringify(), "-1000.000000 + -1000.000000");
 }
 
 TEST(SubTest, SubEvaluatePositive) {
