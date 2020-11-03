d81e91ed869fa1ed183e103be8f1e79040d3a084
Danz Moses <danzmoses@gmail.com>
2020-11-02 19:03:58 -0800

    Fixed main.cpp to run until completion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "op.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "pow.hpp"
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
