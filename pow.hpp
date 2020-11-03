0246a02272a93233548a9edb5b8574d2b69ee98b
Danz Moses <danzmoses@gmail.com>
2020-11-02 18:31:18 -0800

    Implemented evaluate() for Pow class

--- pow.hpp
+++ pow.hpp
@@ -9,7 +9,23 @@ class Pow : public Base {
         Base* power;
     public:
         Pow(Base* base, Base* power) : base(base), power(power), Base() {  }
-        virtual double evaluate() { }
+        virtual double evaluate()
+        {
+            double result = 1;          // result starts at 1
+
+            if (power->getValue() < 0)  // if negative power
+            {
+                double positive_power = power->getValue() * -1;         // number of times to divide (magnitude of power)
+                for (unsigned int i = 0; i < positive_power; ++i)
+                    result /= base->getValue();
+            }
+            else                        // if zero or positive power
+            {
+                for (unsigned int i = 0; i < power->getValue(); ++i)    // number of times to multiply
+                    result *= base->getValue();
+            }
+            return result;
+        }
         virtual std::string stringify() { }
 };
 
