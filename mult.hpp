f890d81e07e3eda611488e995ab5b8e7ccf91bac
roycemoses <royce.kmoses@gmail.com>
2020-11-02 17:40:06 -0800

    Implemented constructor function to use THIS base's setValue()

--- mult.hpp
+++ mult.hpp
@@ -8,7 +8,7 @@ class Mult : public Base {
         Base* value1;
         Base* value2;
     public:
-        Mult(Base* value1, Base* value2) : value1(value1), value2(value2), Base() { }
+        Mult(Base* value1, Base* value2) : value1(value1), value2(value2), Base() { this->setValue(evaluate()); }
         virtual double evaluate() { return value1->getValue() * value2->getValue(); }
         virtual std::string stringify() { return std::to_string(value1->getValue()) + std::string(" * ") + std::to_string(value2->getValue()); }
 };
