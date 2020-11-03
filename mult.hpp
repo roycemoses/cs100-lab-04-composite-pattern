eec34ce4baa85393a5d57bedbc53348d8ecb9c75
roycemoses <royce.kmoses@gmail.com>
2020-11-02 17:38:29 -0800

    Implemented evaluate() function

--- mult.hpp
+++ mult.hpp
@@ -9,6 +9,7 @@ class Mult : public Base {
         Base* value2;
     public:
         Mult(Base* value1, Base* value2) : value1(value1), value2(value2), Base() { }
+        virtual double evaluate() { return value1->getValue() * value2->getValue(); }
 };
 
 #endif // __MULT_HPP__
