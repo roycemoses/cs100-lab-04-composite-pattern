1908702e78f463d9652c4eae26184841359a0815
Danz Moses <danzmoses@gmail.com>
2020-11-03 18:47:20 -0800

    Fixed evaluate() for Pow class

--- pow.hpp
+++ pow.hpp
@@ -2,6 +2,8 @@
 #define __POW_HPP__
 
 #include "base.hpp"
+#include <cmath>
+
 
 class Pow : public Base {
     private:
@@ -9,23 +11,7 @@ class Pow : public Base {
         Base* power;
     public:
         Pow(Base* base, Base* power) : base(base), power(power), Base() { this->setValue(evaluate()); }
-        virtual double evaluate()
-        {
-            double result = 1;          // result starts at 1
-
-            if (power->getValue() < 0)  // if negative power
-            {
-                double positive_power = power->getValue() * -1;         // number of times to divide (magnitude of power)
-                for (unsigned int i = 0; i < positive_power; ++i)
-                    result /= base->getValue();
-            }
-            else                        // if zero or positive power
-            {
-                for (unsigned int i = 0; i < power->getValue(); ++i)    // number of times to multiply
-                    result *= base->getValue();
-            }
-            return result;
-        }
+        virtual double evaluate() { return pow(base->getValue(), power->getValue()); }
         virtual std::string stringify() { return base->stringify() + std::string(" ** ") + power->stringify(); }
 };
 
